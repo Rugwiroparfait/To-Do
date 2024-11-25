d6ad408df1057bf6f82fea6dc71d77f5b98a762e
Rugwiroparfait <rugwiroparfait003@gmail.com>
2024-11-25 23:02:23 +0200

    added static files for login template

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('todo.urls')),
+    path('login/', LoginView.as_view(template_name='todo/login.html'), name='login'),
 ]
